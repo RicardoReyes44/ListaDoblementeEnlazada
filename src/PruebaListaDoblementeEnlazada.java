a1971c02afddfa5e290fc0e9449c0d80099bea30
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-06 01:07:03 -0600

    Se agrego la clase 'Nodo' con sus atributos 'nodoAnterior', 'nodoSiguiente' y 'dato' junto con sus setters y getters

--- src/PruebaListaDoblementeEnlazada.java
+++ src/PruebaListaDoblementeEnlazada.java
@@ -1,2 +1,55 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+class Nodo {
+	
+	private Nodo nodoAnterior;
+	private Nodo nodoSiguiente;
+	private int dato;
 
+	public Nodo(int dato) {
+		nodoAnterior = nodoSiguiente = null; // Opcional
+		this.dato = dato;
+	}
+
+	public Nodo getNodoAnterior() {
+		return nodoAnterior;
+	}
+
+	public void setNodoAnterior(Nodo nodoAnterior) {
+		this.nodoAnterior = nodoAnterior;
+	}
+
+	public Nodo getNodoSiguiente() {
+		return nodoSiguiente;
+	}
+
+	public void setNodoSiguiente(Nodo nodoSiguiente) {
+		this.nodoSiguiente = nodoSiguiente;
+	}
+
+	public int getDato() {
+		return dato;
+	}
+
+	public void setDato(int dato) {
+		this.dato = dato;
+	}
+
+	@Override
+	public String toString() {
+		return "Nodo [dato=" + dato + "]";
+	}
+	
+}
+
+/*
+ * OPERACIONES BASICAS
+ *   1) Creacion
+ *   2) Insercion
+ *     2a)Al inicio
+ *     2b)Al final
+ *   3) Eliminacion
+ *     3a)Al inicio
+ *     3b)Al final
+ * */
